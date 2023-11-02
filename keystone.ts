69a4efa95ab254a2a53352e93927612bf0ec79c1
BasedRed <gussmandva@gmail.com>
2023-11-02 09:28:06 +0200

    minor change

--- keystone.ts
+++ keystone.ts
@@ -17,6 +17,7 @@ import { withAuth, session } from './auth';
 export default withAuth(
     config({
         db: {
+            useMigrations: true,
             // we're using sqlite for the fastest startup experience
             //   for more information on what database might be appropriate for you
             //   see https://keystonejs.com/docs/guides/choosing-a-database#title
